2991f2654857c296ccb48b37ef24b9a4ee214b77
Hazem Yasser ElSayed Hassan <Hazem.hassan04@eng-st.cu.edu.eg>
2026-01-17 16:27:22 +0200

    Add allowedHosts to Vite server configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,     
-    host: true    
+    allowedHosts: [
+      'hazem-yasser.francecentral.cloudapp.azure.com'
+    ],
+    host: true,          // allows external access
+    port: 5173,          // keep default (optional)
   }
 })
